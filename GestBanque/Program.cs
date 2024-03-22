5a61048efc0f55014db3b4e27dc2dbceae211e20
ArthurDC <arthurbary@example.com>
2024-03-22 14:05:34 +0100

    progamme with new data

--- GestBanque/Program.cs
+++ GestBanque/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Models;
 
+Banque bbl = new Banque();
+
 Personne doeJhon = new Personne()
 {
     Nom = "Doe",
@@ -14,3 +16,14 @@ Personne doeJane = new Personne()
     Prenom = "Jane",
     DateNaiss = new DateTime(1980, 3, 2)
 };
+
+Courant compte = new Courant()
+{
+    Titulaire = doeJane,
+    Numero = "US1234-5678-1234"
+};
+
+compte.Depot(1000000);
+
+bbl.Ajouter(compte);
+ 
