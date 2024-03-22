3922659618a34026f762487a58732f3b2669207b
ArthurDC <arthurbary@example.com>
2024-03-22 09:53:15 +0100

    before correction

--- Models/Courant.cs
+++ Models/Courant.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,19 +10,50 @@ namespace Models
     public class Courant
     {
         public string Numero { get; set; }
-        public double Sold { get; private set; }
-        public double LigneDeCredit {  get; set; }
         public Personne Titulaire { get; set; }
-
-        public void Retrait(double Montant)
-        {
-            Console.WriteLine();
+        public double Sold { get; private set; }
+        
+        private double _ligneDeCredit;
+        public double LigneDeCredit {  get
+            {
+                return _ligneDeCredit;
+            }
+            set 
+            {
+                if( _ligneDeCredit < 0 )
+                {
+                    Console.WriteLine("La ligne de credit doit etre un chiffre positif ou zéro");
+                } else
+                {
+                    _ligneDeCredit = value;
+                }
+            }
         }
 
-        public void Depot(double Montant)
+        public void Retrait(double montant)
         {
-            Console.WriteLine();
+            if(montant > Sold) {
+                Console.WriteLine($"Le montant {montant} est superdieur avotre solde({Sold})");
+            } else if (montant < 1) {
+                Console.WriteLine($"Le montant {montant} n'est pas valid");
+            } else
+            {
+                Sold -= montant;
+                Console.WriteLine($"Le montant {montant} a ete retiré sur votre compte ${Sold}");
+            }
         }
 
+        public void Depot(double montant)
+        {
+            if (montant < 1)
+            {
+                Console.WriteLine($"Le montant {montant} n'est pas valid");
+            }
+            else
+            {
+                Sold += montant;
+                Console.WriteLine($"Le montant {montant} a ete versé sur votre compte ${Sold}");
+            }
+        }
     }
 }
