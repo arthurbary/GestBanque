c0f70ab6afb24a7ee7081736dfb57fe22f57800f
ArthurDC <arthurbary@example.com>
2024-03-22 10:51:47 +0100

    small changes

--- GestBanque/Program.cs
+++ GestBanque/Program.cs
@@ -1,8 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Models;
 
-Personne doeJhon = new Personne();
+Personne doeJhon = new Personne()
 {
     Nom = "Doe",
+    Prenom = "Jhon",
+    DateNaiss = new DateTime(1980, 3, 2)
+};
 
-}
+Personne doeJane = new Personne()
+{
+    Nom = "Doe",
+    Prenom = "Jane",
+    DateNaiss = new DateTime(1980, 3, 2)
+};
