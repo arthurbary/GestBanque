da89ea79c0a6479bd3a349c1545525f1e2619c36
ArthurDC <arthurbary@example.com>
2024-03-22 10:50:24 +0100

    add some people

--- GestBanque/Program.cs
+++ GestBanque/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Models;
+
+Personne doeJhon = new Personne();
+{
+    Nom = "Doe",
+
+}
--- Models/Personne.cs
+++ Models/Personne.cs
@@ -11,5 +11,19 @@ namespace Models
         public string Nom { get; set; }
         public string Prenom { get; set; }
         public DateTime DateNaiss { get; set; }
+        
+        private string _test;
+
+        public string Test
+        {
+            get 
+            {
+                return _test;
+            }
+            set
+            {
+                _test = value;
+            }
+        }
     }
 }
